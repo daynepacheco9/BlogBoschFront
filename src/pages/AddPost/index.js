c2c8715be1195777f5a0e2a4e4646fce035e795a
juupurcino <jdiaspurcino@gmail.com>
2024-09-02 11:14:54 -0300

    add front que segui pdf

---
+++ src/pages/AddPost/index.js
@@ -0,0 +1,10 @@
+import Formulario from "../../components/Formulario";
+import NavBar from "../../components/NavBar";
+export default function AddPostPage() {
+    return (
+        <>
+            <NavBar />
+            <Formulario />
+        </>
+    )
+}
